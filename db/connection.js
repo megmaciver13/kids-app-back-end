6e469b4910f41d0c858721f85f0a442f378dbfac
Meg MacIver <megmaciver13@gmail.com>
2017-12-19 12:42:36 -0500

    set up mlab in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/kidsapp', { useMongoClient: true })
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/kidsapp', { useMongoClient: true })
+}
 
 mongoose.Promise = Promise
 
