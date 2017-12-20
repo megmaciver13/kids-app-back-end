bdde01509a9471ad1d728f92a6bbe65920679b2d
Meg MacIver <megmaciver13@gmail.com>
2017-12-20 13:59:49 -0500

    fix functionality to create lesson (works!), working on functionality to create question (currently server crashes, and answers dont save)

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.get('/subjects/:id', (req, res) => {
 app.get('/subjects/:subject_id/lesson/:id', (req, res) => {
   Subject.findOne({ _id: req.params.subject_id })
     .then(subject => {
-      console.log(subject.lessons, req.params.id)
       let lesson = subject.lessons.find((lesson) => {
         return lesson._id == req.params.id
       })
@@ -51,6 +50,46 @@ app.get('/subjects/:subject_id/lesson/:id', (req, res) => {
     .catch(err => {
       console.log(err)
     })
+})
+
+app.post('/subjects/:id', (req, res) => {
+  Subject.findById(req.params.id)
+    .then(subject => {
+      subject.lessons.push({name: req.body.name, lessonImage: req.body.lessonImage, questions: []})
+      console.log(subject)
+      subject.save((err, subject) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.json(subject)
+        }
+      })
+    })
+    .catch(err => console.log(err))
+})
+
+app.post('/subjects/:subject_id/lesson/:id', (req, res) => {
+  Subject.findById(req.params.subject_id)
+    .then(subject => {
+      console.log(subject)
+      let lesson = subject.lessons.find(lesson => {
+        return lesson._id == req.params.id
+      })
+      console.log(lesson)
+      let newAnswer1 = {image: req.body.image1, text: req.body.text1, isCorrect: req.body.isCorrect1}
+      let newAnswer2 = {image: req.body.image2, text: req.body.text2, isCorrect: req.body.isCorrect2}
+      let newAnswer3 = {image: req.body.image3, text: req.body.text3, isCorrect: req.body.isCorrect3}
+      lesson.questions.push({question: req.body.question, answers: [newAnswer1, newAnswer2, newAnswer3]})
+      subject.save((err, subject) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.json(subject)
+        }
+      })
+      res.json(lesson)
+    })
+    .catch(err => console.log(err))
 })
 
 app.listen(process.env.PORT || 3001)
